5c2b43ba87412cba83f1e88eca1040e20f8dadde
Jeff <wrathallj@gmail.com>
2018-07-11 14:07:12 -0400

    styled component

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React,{Component} from 'react';
 import ShowCard from './ShowCard';
 import preload from '../data.json';
 
+// must have a render method returing markup
+class Search extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      searchTerm:'this is a debug statement'
+    }
+    //provides the correct context for the state and the change
+    this.handleSearchChange = this.handleSearchChange.bind(this);
+  }
 
-const Search = () => (
-    <div className="search">
+  handleSearchChange(event){
+    this.setState({searchTerm:event.target.value});
+  }
+
+  render(){
+    return  <div className="search">
+      <header>
+        <h1>Video Search Service</h1>
+        <input type='text' value ={this.state.searchTerm} placeholder='Search' onChange={this.handleSearchChange}/>
+      </header> 
       <div>
-        {preload.shows.map(show => <ShowCard show={show} />)}
-        
+        {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)} 
       </div>
     </div>
-  );
+  }
+}  
 export default Search;
